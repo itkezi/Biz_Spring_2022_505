d4f9088684d48d84a9f6655f3a56f95c4992b1e9
itkezi <dkdlel816@naver.com>
2022-07-27 15:12:12 +0900

    220727

---
+++ SpMVC_040_SecurityV2/src/main/java/com/callor/sec/model/userVO.java
@@ -0,0 +1,35 @@
+package com.callor.sec.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/*
+ * Security 에서 제공하는 사용자 정보를 담을 VO를 생성한다
+ * 여기서 UserDetails 인터페이스를 상속받아 사용한다
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class userVO implements UserDetails{
+
+	private String username;
+	private String password;
+	private boolean isAccountNonExpired;
+	private boolean isAccountNonLocked;
+	private boolean isCredentailsNonExpired;
+	
+	private Collection<? extends GrantedAuthority> authorites;
+	
+	
+
+}
